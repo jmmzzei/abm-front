e26ad618b2d463dd1783dd378fa00278d3816fa0
José María <josemmazzei@gmail.com>
2021-01-17 21:51:18 -0300

    Create companies service

---
+++ src/service/companies.service.js
@@ -0,0 +1,28 @@
+import API from './config.service'
+
+const companies = '/companies/'
+
+export const companiesService = {
+  getById: id =>
+    new Promise((resolve, reject) => {
+      API.get(companies + id)
+        .then(res => {
+          resolve(res.data)
+        })
+        .catch(err => {
+          reject(err)
+        })
+    }),
+
+  patch: (id, data) =>
+    new Promise((resolve, reject) => {
+      API.patch(companies + id, data)
+        .then(res => {
+          resolve(res.data)
+          console.log(res.data)
+        })
+        .catch(err => {
+          reject(err)
+        })
+    }),
+}
